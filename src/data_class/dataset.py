042072894c5709b7919c7b7da6a024135038d637
mikcl <mikesmikes400@gmail.com>
2022-05-25 19:11:56 +0100

    fix - update Dataset

--- src/data_class/dataset.py
+++ src/data_class/dataset.py
@@ -3,3 +3,6 @@ from .dataclass import DataClass
 class Dataset(DataClass):
     file_name: str = "out.tensor"
     classes: int = 256
+    num_workers: int = 4
+    pin_memory: bool = False
+    prefetch_factor: int = 256  # 256 (Prefetch) * 8 (Long) * 2048 (GPT context) * 256 (High Batch) = 1GiB RAM
